41793446d00d61d192887a203a4528e438c6d0b1
Muhammad Helmy Fadillah <helmy.fadillah.work@gmail.com>
2024-10-28 18:47:40 +0700

    fix bug in carousel page

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -6,7 +6,7 @@ import { useMediaQuery } from 'react-responsive';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-import 'swiper/css/autoplay'; // Import the autoplay style
+import 'swiper/css/autoplay';
 
 import './style.css';
 
@@ -16,15 +16,11 @@ import { Pagination, Autoplay } from 'swiper/modules';
 export default function Carousel() {
   // Media query checks
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-  const isTabletOrLaptop = useMediaQuery({ query: '(min-width: 769px) and (max-width: 1024px)' });
 
   // Set the image source based on the device type
-  const imageSrc = isMobile
-    ? '/bannermobile.png'
-    : '/banner1.png'; // Default for tablet and larger screens
-
-  const imageWidth = isMobile ? 400 : 1000; // Width adjustments for mobile and larger screens
-  const imageHeight = isMobile ? 200 : 400; // Height adjustments for mobile and larger screens
+  const imageSrc = isMobile ? '/bannermobile.png' : '/banner1.png';
+  const imageWidth = isMobile ? 400 : 1000;
+  const imageHeight = isMobile ? 200 : 400;
 
   return (
     <>
@@ -36,8 +32,8 @@ export default function Carousel() {
           loop={true}
           modules={[Pagination, Autoplay]}
           autoplay={{
-            delay: 5000, // Slide every 5 seconds
-            disableOnInteraction: false, // Keep autoplay after user interaction
+            delay: 5000,
+            disableOnInteraction: false,
           }}
           className="h-[22rem] md:h-96"
         >
